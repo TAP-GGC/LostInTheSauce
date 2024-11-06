0cfc8d3b87e9402ae4b8ee9844b1f49ce98c75ef
AdisMahmic <158762612+AdisMahmic@users.noreply.github.com>
2024-11-05 19:55:28 -0500

    pushing all unity files to the beginning

---
+++ code/Assets/Scripts/Gameplay/DestroyerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerScript : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform prefabPosition;
+
+    PointScript ps = new PointScript();
+    public int minY;
+    public int maxY = -2;
+
+    void Start()
+    {
+        //prefab = GetComponent<GameObject>();
+
+    }
+
+    void Update()
+    {
+        if (prefabPosition.position.y > maxY)
+        {
+            maxY = (int) prefabPosition.position.y;
+        }
+            minY = maxY - 3;
+        if (prefabPosition.position.y < minY)
+        {
+            Destroy(prefab);
+            //PlayerPrefs.SetInt("PrefabCount", PlayerPrefs.GetInt("PrefabCount") - 1);
+            //PlayerPrefs.SetString("PlayerDestroyed", "true");
+        }
+    }
+}
---
+++ code/Assets/Scripts/Gameplay/SpawnIceCream.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnIceCream : MonoBehaviour
+{
+    public GameObject iceCream;
+    public GameObject gold;
+    public Transform playerPos;
+    public int itemYPos;
+    public int maxY = -2;
+    public int goldYSpawn = 8;
+    public bool isWut;
+    public bool isMainMenu;
+    public bool isTimed;//Comment out to see it disappear in Inspector
+    private float timeCount = 0.0f;
+    private float timeThreshold = 0.0f;
+    private int totalIceCream;
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("PrefabCount", 0);
+    }
+    private void createIceCream()//x = -5/4
+    {
+        if (isWut)
+        {
+            itemYPos = (int)playerPos.position.y + 10;
+            int randNum = Random.Range(-5, 4);
+            GameObject item = Instantiate(iceCream, new Vector3Int(randNum, itemYPos, 0), Quaternion.identity) as GameObject;
+            PlayerPrefs.SetInt("PrefabCount", PlayerPrefs.GetInt("PrefabCount") + 1);
+        }
+        if(isMainMenu)
+        {
+            int randNum = Random.Range(-150, 150);
+            GameObject item = Instantiate(iceCream, new Vector3Int(randNum, 120, 91), Quaternion.identity) as GameObject;
+        }
+    }
+
+    private void createGold()//x = -5/4
+    {
+        itemYPos = (int)playerPos.position.y + 10;
+        int randNum = Random.Range(-5, 4);
+        GameObject item = Instantiate(gold, new Vector3Int(randNum, itemYPos, 0), Quaternion.identity) as GameObject;
+    }
+
+    void Update()
+    {
+        if (isWut)//If the camera is in the playing area, then perform these methods/do these
+        {
+            timeCount = Time.time;
+            if (timeCount > timeThreshold)//Creates ice cream every 0.5 seconds
+            {
+                //createIceCream();//Uncomment out to enable
+                timeThreshold += 0.5f;//Change the timeThreshold to change the time Ice Cream spawns
+            }
+            //createIceCream();//Keeps creating ice cream
+            if ((int)playerPos.position.y > maxY)//Only creates ice cream when player goes above their highest point
+            {
+                maxY = (int)playerPos.position.y;
+                createIceCream();
+            }
+            if((int)playerPos.position.y > goldYSpawn)//Creates gold every 10 walls
+            {
+                goldYSpawn += 10;
+                createGold();
+            }
+        }
+
+        
+        if (isTimed)//If the camera is in the playing area, then perform these methods/do these
+        {
+            if (PlayerPrefs.GetInt("PrefabCount") <= 4)//If there is less than or 4 Ice Cream, then make another Ice Cream
+                //prefab. In other words, there can only be 5 Ice Cream on the screen at a time(ISV2)
+            {
+                timeCount = Time.time;
+                if (timeCount > timeThreshold)//Creates ice cream every 0.5 seconds
+                {
+                    createIceCream();
+                    timeThreshold += 0.5f;//Change the timeThreshold to change the time Ice Cream spawns
+                }
+                //createIceCream();//Keeps creating ice cream
+                if ((int)playerPos.position.y > maxY)//Only creates ice cream when player goes above their highest point
+                {
+                    maxY = (int)playerPos.position.y;
+                    createIceCream();
+                }
+            }
+            if ((int)playerPos.position.y > goldYSpawn)//Creates gold every 10 walls
+            {
+                goldYSpawn += 10;
+                createGold();
+            }
+        }
+        
+        
+
+        if (isMainMenu)//If the camera is in the Main Menu, then perform these methods/do these
+        {
+            createIceCream();
+        }
+    }
+}
