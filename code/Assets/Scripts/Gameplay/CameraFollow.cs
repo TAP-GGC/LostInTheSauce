c666868e3d1a766e9e94d4c8509d865c574e442c
AdisMahmic <158762612+AdisMahmic@users.noreply.github.com>
2024-11-24 12:50:27 -0500

    Update CameraFollow.cs
    commenting code

--- code/Assets/Scripts/Gameplay/CameraFollow.cs
+++ code/Assets/Scripts/Gameplay/CameraFollow.cs
@@ -7,12 +7,15 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
+
+    // Tracks the maximum vertical position the player has reached
+    // Used to control the vertical movement of the camera.
     public float maxY = -2.0f;
 
     void FixedUpdate()
     {
         //transform.position = new Vector3(0, player.position.y, transform.position.z);//Normal Camera Movement
-        transform.position = new Vector3(0, maxY + 3.5f, transform.position.z);//Vertical Camera Movement
+        transform.position = new Vector3(0, maxY + 3.5f, transform.position.z);//Vertical Camera Movement with no horisontal movement to ensure smooth camera tracking upwards.
         if (player.position.y > maxY)
         {
             maxY = player.position.y;
